51379279e4b736640decf20e5127ff8a8cc5f83e
evgenii_krysenko <evgenii_krysenko@epam.com>
2018-11-27 16:20:04 +0300

    Change cb2xml module structure Add gradle.build (works properly)

--- cb2xml/src/net/sf/cb2xml/def/DialectManager.java
+++
@@ -1,8 +0,0 @@
-package net.sf.cb2xml.def;
-
-public class DialectManager {
-	public static final NumericDefinition MAINFRAME_NUMERIC_DEFINITION = new BasicNumericDefinition(
-			"Mainframe", BasicNumericDefinition.MAINFRAME_SIZES, BasicNumericDefinition.MAINFRAME_SYNC, false, 4 ,4
-	);
-
-}
